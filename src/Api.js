d77c1e00b5c130ab93649fe8215e0e4afd911c9b
Zeynep Senyurek <zeynep.senyurek@outlook.com>
2022-07-24 15:38:28 +0300

    env file created

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "2cf9261ffdmsh6e6aa128ee87fb9p1c5414jsn1482836a48a4",
+    "X-RapidAPI-Key": process.env.GEO_API_OPTIONS_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -11,7 +11,7 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const searchDestinationApi = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9c533b3396msh469c1a079631799p1f863bjsnc397d509caae",
+    "X-RapidAPI-Key": process.env.SEARCH_DESTINATION_API_KEY,
     "X-RapidAPI-Host": "airbnb19.p.rapidapi.com",
   },
 };
@@ -21,7 +21,7 @@ export const DESTINATION_API_URL =
 export const searchPropertyByPlace = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "9c533b3396msh469c1a079631799p1f863bjsnc397d509caae",
+    "X-RapidAPI-Key": process.env.SEARCH_PROPERTY_BY_PLACE_KEY,
     "X-RapidAPI-Host": "airbnb19.p.rapidapi.com",
   },
 };
