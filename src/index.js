a15cf6741e51fc0579c09b67422cf79ad469acd5
Zeynep Senyurek <zeynep.senyurek@outlook.com>
2022-08-15 23:52:47 +0300

    Update version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <ContextProvider>
+//       <Router>
+//         <App />
+//       </Router>
+//     </ContextProvider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import ContextProvider from "./contexts/AppContext";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <Router>
         <App />
       </Router>
     </ContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
